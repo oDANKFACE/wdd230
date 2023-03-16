6e060ec445f59e2178f0d58c6b5a418fd78b1897
oDANKFACE <darien.r.weller@gmail.com>
2023-03-15 20:15:11 -0600

    Week 10
    Completed week 10 - Using the Weather API to specifications.

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,10 +1,11 @@
 
 
-const temp = Number(document.querySelector('#weather-degrees').innerHTML);
-const windSpeed = Number(document.querySelector('#wind-speed').innerHTML);
+const temp = document.querySelector('#current-temp').innerHTML;
+const windSpeed = document.querySelector('#wind-speed').innerHTML;
 const windChill = document.querySelector('#wind-chill');
 
 let windChillVal = "NA";
+console.log(temp, windSpeed)
 
 if (temp <= 50 && windSpeed > 3) {
     windChillVal = String((35.74 + (0.6215 * temp) - (35.75 * (windSpeed ** 0.16)) + (0.4275 * temp * (windSpeed ** 0.16))).toFixed(2));
